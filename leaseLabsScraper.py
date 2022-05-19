596a12c25cae37c941fc2de2bbce1151516b2f48
Ariella Saslafsky <asaslafsky@Ariellas-MacBook-Air.local>
2022-05-19 16:15:38 -0400

    Finished making the web scrapers. Made a jupyter notebook to start looking at the data.

---
+++ leaseLabsScraper.py
@@ -0,0 +1,136 @@
+from os import lseek
+from playwright.sync_api import sync_playwright
+import time
+import re
+from datetime import datetime
+from os.path import exists
+import csv
+
+
+def clickOnCalendar(frame):
+    frame.click("#calendarInput")
+
+
+def grabAllDaysForMonth(frame):
+    return frame.query_selector_all(".uib-day ")
+
+
+def isClickable(day):
+    return str(day.query_selector("button").get_property('disabled')) == 'false'
+
+
+def isInCurrentMonth(day):
+    return day.query_selector("button .text-muted") is None
+
+
+def clickNextMonth(frame):
+    return frame.query_selector('.glyphicon-chevron-right').click()
+
+
+def getText(jshandle, selector):
+    return str(jshandle.query_selector(selector).get_property('innerText')).replace(',', '')
+
+
+def grabAptsInfo(frame, aptsInfo, currentDateTime, moveInDate):
+    # For Every Floorplan Shown On This Calendar Day
+    for floorplan in frame.query_selector_all(".floorplan-tile"):
+        # If There Isn't A Number In The String, No Apartments With This Floorplan Are Available
+        # So Go To The Next Floorplan
+        isntAvailable = re.search('\d', getText(floorplan, '.primary')) is None
+        if isntAvailable:
+            continue
+
+        floorplanName = getText(floorplan, ".name")
+        bedBathSquareFootage = [int(word) for word in getText(floorplan, ".specs").split() if word.isdigit()]
+        
+        rentRange = re.sub("[^0-9\-]", "",getText(floorplan, '.range')).split('-')
+        if len(rentRange) == 1:
+            rentRange.append(rentRange[0])
+        
+        aptsInfo.append([*currentDateTime, floorplanName, *bedBathSquareFootage, *rentRange, *moveInDate])
+
+    return aptsInfo
+
+
+def saveInfo(aptInfo, fileName):
+    openMode = 'a' if exists(fileName) else 'w'
+    with open(fileName, openMode) as openFile:
+        fileWriter = csv.writer(openFile)
+        columnNames = ['scraping_date', 'scraping_time', 'floorplan_name', 'bedroom', 'bathroom', 'apartment_number', 'rent_min', 'rent_max', 'move_in_month', 'move_in_day', 'move_in_year']
+        if openMode == 'w':
+            fileWriter.writerow(columnNames)
+        fileWriter.writerows(aptInfo)
+
+
+def run(playwright, url, fileName):
+    aptsInfo = []
+    currentDateTime = [datetime.now().strftime("%Y/%m/%d"), datetime.now().strftime("%H")]
+
+    try:
+        # Get Frame Info
+        browser = playwright.firefox.launch(headless=False)
+        page = browser.new_page()
+        page.goto(url)
+        frame = page.wait_for_selector("#rp-leasing-widget").content_frame()
+        
+        # Enter Into Frame
+        frame.click("text=Start")
+
+        # Go Through All The Available Days In The Calendar And Grab Their Information
+        isntFirstMonth = False
+        thereIsMoreInfo = True
+        while thereIsMoreInfo:
+            
+            # Open The Calendar
+            clickOnCalendar(frame)
+            if isntFirstMonth:
+                clickNextMonth(frame)
+            calendarMonth = grabAllDaysForMonth(frame)
+            # Close The Calendar
+            clickOnCalendar(frame)
+            
+            availableDaysForCurrentMonth = []
+            for i, calendarDay in enumerate(calendarMonth):
+                if isClickable(calendarDay) and isInCurrentMonth(calendarDay):
+                    availableDaysForCurrentMonth.append(i)
+            
+            if len(availableDaysForCurrentMonth) == 0:
+                thereIsMoreInfo = False
+
+            isFirstDayOfMonth = True
+            for daysIndex in availableDaysForCurrentMonth:
+                clickOnCalendar(frame)
+                if isntFirstMonth and isFirstDayOfMonth:
+                    clickNextMonth(frame)
+                isFirstDayOfMonth = False
+
+                grabAllDaysForMonth(frame)[daysIndex].click()
+                moveInDate = str(frame.query_selector("#calendarInput").get_property('value')).replace(',', '').split(' ')
+                moveInDate[0] = datetime.strptime(moveInDate[0], "%B").month
+                aptsInfo = grabAptsInfo(frame, aptsInfo, currentDateTime, moveInDate[:3])
+                
+            
+            isntFirstMonth = True
+        saveInfo(aptsInfo, fileName)
+
+    except Exception as e:
+        time.sleep(4)
+        raise(e)
+    finally:
+        page.close()
+        browser.close()
+
+
+def runPlaywright(url, fileName):
+    with sync_playwright() as playwright:
+        run(playwright, url, fileName)
+
+if __name__ == '__main__':
+    calibreUrl = "https://87022266.onlineleasing.realpage.com"
+    calibreFileName = "infoCalibre.csv"
+
+    legacyURL = "https://4548768.onlineleasing.realpage.com"
+    legacyFileName = "infoLegacy.csv"
+
+    runPlaywright(calibreUrl, calibreFileName)
+    
---
+++ madisonDruidHillsScraper.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from os.path import exists
+import os
+import csv
+from datetime import datetime
+
+wordToNum = {'one': 1, 'two': 2, 'three': 3, 'four': 4}
+
+
+def getApartmentNumericChars(apt, selector):
+    return re.sub("[^0-9\-]", "", apt.select_one(selector).get_text())
+
+
+def saveInfo(aptInfo, fileName):
+    openMode = 'a' if exists(fileName) else 'w'
+    with open(fileName, openMode) as openFile:
+        fileWriter = csv.writer(openFile)
+        columnNames = ['scraping_date', 'scraping_time', 'floorplan_name', 'bedroom', 'bathroom', 'apartment_number', 'square_footage', 'rent_min', 'rent_max', 'move_in_month', 'move_in_day', 'move_in_year']
+        if openMode == 'w':
+            fileWriter.writerow(columnNames)
+        fileWriter.writerows(aptInfo)
+
+def runMadisonScraper(fileName):
+    currentDate = datetime.now().strftime("%Y/%m/%d")
+    currentTime = datetime.now().strftime("%H")
+
+    url = "https://www.madisondruidhills.com/floorplans/availableunits?handler=Filter"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    floorplanInfo = soup.select(".mb-3")
+    apartmentsForFloorplan = soup.select(".table-tab-widget")
+
+    aptsInfo = []
+    for floorplan, floorplanApts in zip(floorplanInfo, apartmentsForFloorplan):
+        sentence = floorplan.get_text().split()
+        floorplanInfo = [wordToNum[sentence[i]] if sentence[i] in wordToNum else sentence[i] for i in [1, 4, 6]]
+
+        # Get Every Apartment
+        apts = floorplanApts.select("[data-selenium-id^=\"urow\"]")
+        
+        for apt in apts:
+            apartmentNumber = getApartmentNumericChars(apt, ".td-card-name")
+            squareFootage = getApartmentNumericChars(apt, ".td-card-sqft")
+            rentRange = getApartmentNumericChars(apt, ".td-card-rent").split('-')
+            moveInDate = apt.button['onclick'].split("MoveInDate=")[-1][:-1].split('/')
+            aptInfo = [currentDate, currentTime, *floorplanInfo, apartmentNumber, squareFootage, *rentRange, *moveInDate]
+            aptsInfo.append(aptInfo)
+
+    saveInfo(aptsInfo, f"{os.getcwd()}/{fileName}")
+
+
+if __name__ == "__main__":
+    fileName = 'infoMadison.csv'
+    runMadisonScraper(fileName)
