31404de32aee54119efe2fdb5ee73489547ebb83
Ariella Saslafsky <asaslafsky@Ariellas-MacBook-Air.local>
2022-05-22 18:11:33 -0400

    Got rid of unnecessary files and added the csvs back in to be tracked

--- runScrapers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 madisonDruidHillsScraper.py 2>&1 | tee -a logs/madison.log
-python3 leaseLabsScraper.py https://87022266.onlineleasing.realpage.com csvs/infoCalibre.csv 2>&1 | tee -a logs/calibre.log
-python3 leaseLabsScraper.py https://4548768.onlineleasing.realpage.com csvs/infoLegacy.csv 2>&1 | tee -a logs/legacy.log
